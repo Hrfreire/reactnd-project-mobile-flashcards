b4afaa02e3ff4539e52fed7c1ae7ed4e3dd9b47d
Hugo Freire <hugorfreire@gmail.com>
2019-03-25 21:39:36 -0300

    feat: remove unused actions

--- actions/index.js
+++ actions/index.js
@@ -4,17 +4,12 @@ import {
   addQuestion as addQuestionApi
 } from '../utils/api'
 
-export const GET_DECK_LIST = 'GET_DECK_LIST'
 export const SUCCESS_GET_DECK_LIST = 'SUCCESS_GET_DECK_LIST'
-export const ADD_DECK = 'ADD_DECK'
 export const SUCCESS_ADD_DECK = 'SUCCESS_ADD_DECK'
-export const ADD_QUESTION = 'ADD_QUESTION'
 export const SUCCESS_ADD_QUESTION = 'SUCCESS_ADD_QUESTION'
 
 export function getDeckList () {
   return async function (dispatch) {
-    dispatch ({ type: GET_DECK_LIST })
-
     const decks = await fetchDeckListApi()
 
     return dispatch( { type: SUCCESS_GET_DECK_LIST, payload: decks })
@@ -23,7 +18,6 @@ export function getDeckList () {
 
 export function addDeck (title) {
   return async function (dispatch) {
-    dispatch ({ type: ADD_DECK })
     await addDeckApi({ title, questions: [] })
 
     return dispatch( { type: SUCCESS_ADD_DECK, payload: { title, questions: [] } })
@@ -32,7 +26,6 @@ export function addDeck (title) {
 
 export function addQuestion (deckTitle, question) {
   return async function (dispatch) {
-    dispatch({ type: ADD_QUESTION })
     const updatedDeck = await addQuestionApi(deckTitle, question)
 
     return dispatch({ type: SUCCESS_ADD_QUESTION, payload: updatedDeck })
